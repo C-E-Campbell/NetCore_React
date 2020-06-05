ed333c4fcbd4adb1f7e9e478e157646df7e67f7e
Charles <charles.e.campbell1989@gmail.com>
2020-06-04 23:34:31 -0400

    add data values

--- CoreReact/Persistence/DataContext.cs
+++ CoreReact/Persistence/DataContext.cs
@@ -11,5 +11,14 @@ namespace Persistence
 
         }
         public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>().HasData(
+                new Value { Id = 1, name = "Value 101" },
+                new Value { Id = 2, name = "Value 102" },
+                new Value { Id = 3, name = "Value 103" }
+            );
+        }
     }
 }
